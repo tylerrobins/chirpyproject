9f54a3a8dcfb67c25d71650308620aaa567bca9d
TylerKingsley <tyler@kingsleyrobins.com>
2024-03-17 21:33:40 +0200

    initial server & / handled

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tylerrobins/chirpyproject
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	// Create a new ServeMux
+	mux := http.NewServeMux()
+
+	// Handle routes
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
+			return
+		}
+		http.ServeFile(w, r, "index.html")
+	})
+
+	corsMux := middlewareCors(mux)
+
+	// Listen and serve
+	http.ListenAndServe(":8080", corsMux)
+
+}
+
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
